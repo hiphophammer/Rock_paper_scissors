b54f15baf8a769c1f0307343397debff56ad797b
hiphophammer <hiphophammer@hotmail.com>
2022-10-07 01:01:58 -0700

    Added function "getComputerChoice()"

---
+++ rps.js
@@ -0,0 +1,15 @@
+/*
+    return: rock/paper/scissors in number
+    Rock: 0
+    Paper: 1
+    Scissors: 2
+*/
+function getComputerChoice() {
+    return Math.random() * 3;
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
