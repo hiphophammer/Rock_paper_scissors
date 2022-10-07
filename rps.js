7bf7310437f7b6edab71e5fd407051c2a48ffb18
hiphophammer <hiphophammer@hotmail.com>
2022-10-07 01:08:36 -0700

    Added skeleton for playGame()

--- rps.js
+++ rps.js
@@ -8,8 +8,15 @@ function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+/*
+    playerSelection: player choice in string
+    computerSelection: computer choice in number
+    Rock: 0
+    Paper: 1
+    Scissors: 2
+    return: result string
+*/
+function playGame(playerSelection, computerSelection) {
+    return "";
+}
+
